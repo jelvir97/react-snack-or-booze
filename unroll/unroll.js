5f50e14bfa5023da666f905be34f78ba47b46210
Juan Elvir <jelvir97@gmail.com>
2024-01-04 12:03:36 -0700

    unroll() and tests)

--- unroll/unroll.js
+++ unroll/unroll.js
@@ -1,5 +1,56 @@
 function unroll(squareArray) {
+const newArr = [];
 
+let topArrIdx = 0;
+let botArrIdx = squareArray.length
+
+let leftIdx = -1;
+let rightIdx = squareArray[0].length -1
+
+let x=0;
+let y=0;
+while( newArr.length < squareArray.length * squareArray[0].length){
+    if(y==topArrIdx){
+        for(let i = x; i <= rightIdx; i++){
+            x = i
+            newArr.push(squareArray[y][x])
+        }
+        botArrIdx-=1
+        leftIdx += 1
+        y+=1
+        continue;
+    }
+
+    if(x==rightIdx && y!==botArrIdx){
+        for(let i = y; i < botArrIdx; i++){
+            y = i
+            newArr.push(squareArray[y][x])
+        }
+        y+=1
+        continue;
+    }
+
+    if(y==botArrIdx && x==rightIdx){
+        for(let i = x; i >= leftIdx; i--){
+            x = i
+            newArr.push(squareArray[y][x])
+        }
+        y -= 1;
+        topArrIdx += 1
+        rightIdx -= 1
+        continue;
+    }
+
+    if(x==leftIdx && y>topArrIdx){
+        for(let i = y; i > topArrIdx; i--){
+            y = i
+            newArr.push(squareArray[y][x])
+        }
+        y-=1
+        continue;
+    }
+}
+return newArr;
 }
 
 module.exports = unroll;
--- unroll/unroll.test.js
+++ unroll/unroll.test.js
@@ -2,8 +2,47 @@ const unroll = require("./unroll");
 
 describe("#unroll", function () {
 
+  const square = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+  ];
+  const smallestSquare = [
+    [1,2],
+    [4,3]
+  ]
+  const smallerSquare = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"]
+  ];
+  
+  unroll(smallerSquare); // ["a", "b", "c", "f", "i", "h", "g", "d", "e"]
+
+  const biggerSquare = [
+    [1,2,3,4,5,6],
+    [20,21,22,23,24,7],
+    [19,32,33,34,25,8],
+    [18,31,36,35,26,9],
+    [17,30,29,28,27,10],
+    [16,15,14,13,12,11]
+]
+
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
+
+  it("should work with squares", ()=>{
+    expect(unroll(square)).toEqual([1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10])
+    expect(unroll(smallerSquare)).toEqual(["a", "b", "c", "f", "i", "h", "g", "d", "e"])
+    expect(unroll(biggerSquare)).toEqual([1, 2, 3, 4, 5, 6,
+                                          7, 8, 9, 10, 11, 12,
+                                          13, 14, 15, 16, 17, 18, 
+                                          19, 20, 21, 22, 23, 24, 
+                                          25, 26, 27, 28, 29, 30, 
+                                          31, 32, 33, 34, 35, 36])
+  })
+  expect(unroll(smallestSquare)).toEqual([1,2,3,4])
 
 });
